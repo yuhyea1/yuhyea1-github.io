dea0f6b452c496b9a8f096a340e2020b884d2b37
yuhyea1 <yuhyea1@gmail.com>
2021-09-23 21:24:15 +0800

    繳交作業上傳

---
+++ sum.js
@@ -0,0 +1,13 @@
+console.log("hello world!");
+
+function sum(param) {
+  let total = 0;
+  for (let i = 1; i <= param; i++) {
+    total = total + i;
+  }
+  return total;
+}
+
+console.log(sum(3)); // 6
+console.log(sum(6)); // 21
+console.log(sum(10)); // 55
